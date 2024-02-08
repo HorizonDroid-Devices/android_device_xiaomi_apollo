d5b4d8a860b249b5673ee00e8a26e9a9ca260ae5
finnaib <shoaibwwe01@gmail.com>
2024-02-09 00:49:13 +0530

    apollo : bringup horizondroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_apollo.mk
+    $(LOCAL_DIR)/aosp_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_apollo-user \
-    cherish_apollo-userdebug \
-    cherish_apollo-eng
+    aosp_apollo-user \
+    aosp_apollo-userdebug \
+    aosp_apollo-eng
--- aosp_apollo.mk
+++ aosp_apollo.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common CHERISH stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common Horizondroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Cherish Flags
+# Horizondroid Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
@@ -25,15 +25,13 @@ TARGET_ENABLE_BLUR := true
 TARGET_INCLUDE_WIFI_EXT := true
 PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_DISABLE_EPPE := true
+TARGET_DEBLOAT := true
 
-# Cherish props
-CHERISH_BUILD_TYPE=OFFICIAL
-CHERISH_MAINTAINER := Finnaib
-CHERISH_CHIPSET := Snapdragon865
-CHERISH_BATTERY := 5000mAh
-CHERISH_DISPLAY := 1080x2400
+# Horizondroid
+CUSTOM_BUILD_TYPE= Official
+HORIZON_MAINTAINER := Finnaib
 
-PRODUCT_NAME := cherish_apollo
+PRODUCT_NAME := aosp_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -45,4 +43,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
-
